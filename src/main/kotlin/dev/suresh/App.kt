b6c3d2250caf421af3086f7f54a9c110f179df3e
Suresh G <sureshg.g@gmail.com>
2022-01-14 17:00:55 -0800

    [skip ci] misc changes

--- src/main/kotlin/dev/suresh/App.kt
+++ src/main/kotlin/dev/suresh/App.kt
@@ -2,15 +2,23 @@ package dev.suresh
 
 import dev.zacsweers.redacted.annotations.*
 import kotlinx.coroutines.*
+import java.lang.Runnable
+import java.net.*
 import java.util.*
 import java.util.concurrent.*
 
 fun main() {
   println("Hello ${Secret("testsecret")}")
+
   val plugins = ServiceLoader.load(Callable::class.java)
   plugins.forEach {
     println(it.call())
   }
+
+  // Load plugins from current directory
+  val loader = URLClassLoader.newInstance(arrayOf(URL("file://${System.getProperty("user.dir")}/plugins.jar")))
+  val svcLoader = ServiceLoader.load(Runnable::class.java,loader)
+  println("Found ${svcLoader.toList().size} plugins!")
 
   runBlocking {
     delay(100)
