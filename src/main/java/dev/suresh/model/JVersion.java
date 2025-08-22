727e8a5ee5b3c0da2bac22b6c024a5ebe1a10ab4
Suresh G <sureshg.g@gmail.com>
2025-08-21 17:03:33 -0700

    chore: dep updates

--- src/main/java/dev/suresh/model/JVersion.java
+++ src/main/java/dev/suresh/model/JVersion.java
@@ -3,12 +3,12 @@ package dev.suresh.model;
 import static java.util.Objects.requireNonNull;
 
 public record JVersion(String name, String version) {
-  public JVersion {
-    requireNonNull(name, "Name is required");
-    requireNonNull(version, "Version is required");
-  }
+    public JVersion {
+        requireNonNull(name, "Name is required");
+        requireNonNull(version, "Version is required");
+    }
 
-  public JVersion() {
-    this("Java", System.getProperty("java.version"));
-  }
+    public JVersion() {
+        this("Java", System.getProperty("java.version"));
+    }
 }
